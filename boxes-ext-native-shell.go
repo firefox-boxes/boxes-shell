d117a614ef3dad64e00e550fcba3307b9993679b
Bill Kudo <bluesky42624@gmail.com>
2020-03-01 13:27:49 -0800

    Rewrote parts of boxes-ext-native-shell to comply with the WebExtensions guide

--- boxes-ext-native-shell.go
+++ boxes-ext-native-shell.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/rpc"
 	"strings"
+	"unsafe"
 )
+
+type Msg struct {
+	msg string
+}
+
+func encodeMessage(msg) ([4]byte, []byte) {
+	b, err := json.Marshal(Msg{msg:msg})
+	if err != nil {
+		panic(err)
+	}
+	bl := uint32(len(b))
+	return (*[4]byte)(unsafe.Pointer(&bl))[:], b
+}
 
 func main() {
 	client, _ := rpc.Dial("tcp", "127.0.0.1:6688")
@@ -13,6 +28,8 @@ func main() {
 	for {
 		cmdString := readInput()
 		cmdString = strings.TrimSuffix(cmdString, "\n")
-		fmt.Println(query(client, cmdString))
+		bl, b := encodeMessage(query(client, cmdString))
+		fmt.Print(bl)
+		fmt.Print(b)
 	}
 }
