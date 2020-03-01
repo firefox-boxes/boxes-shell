14e0254b7ae288044465c0f9c4dae523aae83605
Bill Kudo <bluesky42624@gmail.com>
2020-03-01 11:58:04 -0800

    Initial Commit

---
+++ Main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/rpc"
+	"os"
+	"strings"
+)
+
+func readInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	str, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	return str
+}
+
+func query(client *rpc.Client, req string) string {
+	var res string
+	client.Call("IPC.Handle", &req, &res)
+	return res
+}
+
+func main() {
+	client, _ := rpc.Dial("tcp", "127.0.0.1:6688")
+	defer client.Close()
+	fmt.Println("boxes-ipc at 127.0.0.1:6688")
+	for {
+		fmt.Print(">>> ")
+		cmdString := readInput()
+		cmdString = strings.TrimSuffix(cmdString, "\n")
+		fmt.Println(query(client, cmdString))
+		fmt.Println()
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/firefox-boxes/boxes-shell
+
+go 1.13
